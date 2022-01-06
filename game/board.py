3afa51bc168a9fdfbeed7d0a266c05180775829b
Sable <sablechilds@yahoo.com>
2022-01-05 22:28:46 -0500

    1. fix isWinner func by removing breaking change 2. remove long list

--- game/board.py
+++ game/board.py
@@ -1,6 +1,6 @@
 class Board():
     def __init__(self):
-        self.board = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
+        self.board = [-1 for _ in range(54)]
         self.recent_move = 0
         self.moves_played = 0
 
@@ -63,72 +63,6 @@ class Board():
         index_dirs = [1, 8, 9, 10]
         for idx in index_dirs:
             furthest = idx * 4
-    
-            if idx == 1:
-            #check for potential horizontal wins separately to prevent false positives;
-            #refactor
-                if self.recent_move in range(9):
-                    if (self.recent_move + furthest) in range(9) and self.board[self.recent_move + furthest] == playerNum:
-                        smallest = self.recent_move
-                        win = self.findHorizontalWins(smallest, playerNum)
-                        return win
-                    if (self.recent_move - furthest) in range(9) and self.board[self.recent_move - furthest] == playerNum:
-                        smallest = furthest
-                        win = self.findHorizontalWins(smallest, playerNum)
-                        return win
-                if self.recent_move in range(9, 18):
-                    if (self.recent_move + furthest) in range(9, 18) and self.board[self.recent_move + furthest] == playerNum:
-                        smallest = self.recent_move 
-                        win = self.findHorizontalWins(smallest, playerNum)
-                        return win
-                    if (self.recent_move - furthest) in range(9, 18) and self.board[self.recent_move - furthest] == playerNum:
-                        smallest = furthest
-                        win = self.findHorizontalWins(smallest, playerNum)
-                        return win
-                    
-                if self.recent_move in range(18, 27):
-                    if (self.recent_move + furthest) in range(18, 27) and self.board[self.recent_move + furthest] == playerNum:
-                        smallest = self.recent_move
-                        win = self.findHorizontalWins(smallest, playerNum)
-                        return win
-                    if (self.recent_move - furthest) in range(18, 27) and self.board[self.recent_move - furthest] == playerNum:
-                        smallest = furthest
-                        win = self.findHorizontalWins(smallest, playerNum)
-                        return win
-                    
-                if self.recent_move in range(27, 36):
-                    if (self.recent_move + furthest) in range(27, 36) and self.board[self.recent_move + furthest] == playerNum:
-                        smallest = self.recent_move
-                        win = self.findHorizontalWins(smallest, playerNum)
-                        return win
-                    if (self.recent_move - furthest) in range(27, 36) and self.board[self.recent_move - furthest] == playerNum:
-                        smallest = furthest
-                        win = self.findHorizontalWins(smallest, playerNum)
-                        return win
-                    
-                if self.recent_move in range(36, 45):
-                    if (self.recent_move + furthest) in range(36, 45) and self.board[self.recent_move + furthest] == playerNum:
-                        smallest = self.recent_move
-                        win = self.findHorizontalWins(smallest, playerNum)
-                        return win
-                    if (self.recent_move - furthest) in range(36, 45) and self.board[self.recent_move - furthest] == playerNum:
-                        smallest = furthest
-                        win = self.findHorizontalWins(smallest, playerNum)
-                        return win
-                  
-                if self.recent_move in range(45, 54):
-                    if (self.recent_move + furthest) in range(45, 54) and self.board[self.recent_move + furthest] == playerNum:
-                        smallest = self.recent_move
-                        win = self.findHorizontalWins(smallest, playerNum)
-                        return win
-                    if (self.recent_move - furthest) in range(45, 54) and self.board[self.recent_move - furthest] == playerNum:
-                        smallest = furthest
-                        win = self.findHorizontalWins(smallest, playerNum)
-                        return win
-                else:
-                    return False
-                    
-            
                 
             if self.recent_move + furthest in range(54) and self.board[self.recent_move + furthest] == playerNum:
                 smallest = self.recent_move
